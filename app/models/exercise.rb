ad667dd53d44abfbe671927b98379c55f778b71f
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-11-05 16:04:56 -0800

    add exercises model validations

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: exercises
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  weight     :integer          not null
+#  sets       :integer          not null
+#  reps       :integer          not null
+#  name       :string           not null
+#  date       :datetime         not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Exercise < ActiveRecord::Base
+  validates :user_id, :name, :weight, :sets, :reps, :date, presence: true
+
+  belongs_to :user
 end
