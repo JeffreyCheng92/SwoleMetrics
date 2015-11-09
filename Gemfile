e2e890b4e5a079a35555164bdadfe5b26a338a78
thatkaiguy <kai438@gmail.com>
2015-11-09 13:44:49 -0800

    faker gem and seed data

--- Gemfile
+++ Gemfile
@@ -54,4 +54,6 @@ group :development, :test do
   gem 'better_errors'
 
   gem 'binding_of_caller'
+
+  gem 'faker'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(
+  :email => 'novak@gmail.com',
+  :password => 'password',
+  :password_confirmation => 'password'
+)
+
+User.create!(
+  :email => 'andy@gmail.com',
+  :password => 'password',
+  :password_confirmation => 'password'
+)
+
+User.create!(
+  :email => 'roger@gmail.com',
+  :password => 'password',
+  :password_confirmation => 'password'
+)
+
+1.upto(3) do |user_id|
+  5.times do |_|
+    Exercise.create!(
+      :user_id => user_id,
+      :name => Faker::Hacker.noun,
+      :weight => Faker::Number.number(2),
+      :sets => Faker::Number.number(2),
+      :reps => Faker::Number.number(3),
+      :date => Faker::Date.backward(5)
+    )
+  end
+end
